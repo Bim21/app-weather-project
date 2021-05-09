4f4aea54fa85cc44f933b99791eeb3970cc2f168
DinhHuyKhanh <khanhdinh141@gmail.com>
2021-05-09 19:32:29 +0700

    login facebook and jwt

--- src/main/java/com/vti/repository/IUserRepository.java
+++ src/main/java/com/vti/repository/IUserRepository.java
@@ -1,6 +1,8 @@
 package com.vti.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,5 +12,8 @@ import com.vti.entity.User;
 public interface IUserRepository extends JpaRepository<User, Character>,JpaSpecificationExecutor<User> {
 	public boolean existsById(String id);
 	public User findById(String id);
+	
+	Optional<User> findByEmail(String email);
+
 	
 }
